3c96be072c5fce42839f38f9eb416bb894e58b58
Kirill <pesterevkv2003@gmail.com>
2025-01-05 12:31:30 +0300

    возможность добавления/ удаления объектов

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,11 @@ import { ref } from 'vue'
 
 const showPassword = ref(false)
 const password = ref()
-const marks = ref('YYY')
-const login = ref('Obama')
-const typeRecord = ['LDAP', 'Локальная']
+const marks = ref('')
+const login = ref('')
+const typeRecords = ['LDAP', 'Локальная']
 const selectedTypeRecord = ref('Локальная')
+const valid = ref<boolean>()
 const passwordRules = {
   required: (value: string) => !!value || 'Required.',
   max: (v: string) => v.length <= 100 || 'Max 100 characters',
@@ -19,31 +20,45 @@ const loginRules = {
   max: (v: string) => v.length <= 100 || 'Max 100 characters',
 }
 interface IAccount {
-  marks: Array<string>
-  recordType: string
-  login: string
-  password: string
+  id: `${string}-${string}-${string}-${string}-${string}`
+  marks: string | null
+  recordType: string | null
+  login: string | null
+  password: string | null
 }
-const accounts = ref<IAccount>()
-const createAccount = () => {}
+const accounts = ref<Array<IAccount>>([])
+const createAccount = () => {
+  const account = {
+    id: self.crypto.randomUUID(),
+    marks: null,
+    recordType: null,
+    login: null,
+    password: null,
+  }
+  accounts.value.push(account)
+}
+const deleteAccount = (id: `${string}-${string}-${string}-${string}-${string}`) => {
+  accounts.value = accounts.value.filter((account) => account.id !== id)
+}
 </script>
 
 <template>
+  {{ accounts }}
   <div class="flex flex-col gap-4">
     <div class="flex gap-4">
       <h1 class="text-2xl font-bold">Учетные записи</h1>
-      <v-btn rounded="xs" variant="outlined">+</v-btn>
+      <v-btn @click="createAccount" rounded="xs" variant="outlined">+</v-btn>
     </div>
     <v-alert
       icon="mdi-information"
       text="Для указания нескольких меток для одной пары логин/пароль используйте разделитель ;"
     ></v-alert>
-    <v-form v-model="valid">
+    <v-form v-for="account in accounts" :key="account.id" v-model="valid">
       <v-container>
         <v-row>
           <v-col cols="12" md="3">
             <v-text-field
-              v-model="marks"
+              v-model="account.marks"
               :counter="50"
               :rules="[markRules.max]"
               label="Метки"
@@ -51,16 +66,19 @@ const createAccount = () => {}
           </v-col>
           <v-col cols="12" md="3">
             <v-select
-              v-model="selectedTypeRecord"
+              v-model="account.recordType"
               clearable
               label="Тип записи"
-              :items="typeRecord"
+              :items="typeRecords"
             ></v-select>
           </v-col>
 
-          <v-col cols="12" :md="[selectedTypeRecord == 'LDAP' ? '5' : '3']">
+          <v-col
+            cols="12"
+            :md="[account.recordType == 'LDAP' || account.recordType == null ? '5' : '3']"
+          >
             <v-text-field
-              v-model="login"
+              v-model="account.login"
               :counter="100"
               :rules="[loginRules.max, loginRules.required]"
               label="Логин"
@@ -68,9 +86,9 @@ const createAccount = () => {}
             ></v-text-field>
           </v-col>
 
-          <v-col v-if="selectedTypeRecord == 'Локальная'" cols="12" md="2">
+          <v-col v-if="account.recordType == 'Локальная'" cols="12" md="2">
             <v-text-field
-              v-model="password"
+              v-model="account.password"
               :append-inner-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
               :rules="[passwordRules.required, passwordRules.max]"
               :type="showPassword ? 'text' : 'password'"
@@ -81,7 +99,11 @@ const createAccount = () => {}
             ></v-text-field>
           </v-col>
           <v-col cols="12" md="1">
-            <v-btn icon="mdi-bucket-outline" variant="text"></v-btn>
+            <v-btn
+              @click="deleteAccount(account.id)"
+              icon="mdi-bucket-outline"
+              variant="text"
+            ></v-btn>
           </v-col>
         </v-row>
       </v-container>
